af65e6779c8f54137c830ef0b1e92767922741da
Francis Ochieng <61507583+Fran-Ck@users.noreply.github.com>
2020-03-04 01:15:25 +0300

    Add files via upload

---
+++ organizer.py
@@ -0,0 +1,31 @@
+from watchdog import observers
+from watchdog.events import PatternMatchingEventHandler, FileSystemEventHandler
+import json
+import time
+import os
+
+
+class FileHandler(FileSystemEventHandler):
+    i = 1
+
+    def on_modified(self, event):
+        for filename in os.listdir(home):
+            source = home + "/" + filename
+            new_folder = destination + "/" + filename
+            os.rename(source, new_folder)
+
+
+home = "/home/franc/Downloads"
+destination = "/home/franc/Music/newfolder"
+
+event_handler = FileHandler()
+observer = observers.Observer()
+observer.schedule(event_handler, home, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(10)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
